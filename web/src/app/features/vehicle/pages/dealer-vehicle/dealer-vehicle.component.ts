f82f150d79dbed011f47ff6d0085f65ff52feaec
Murphy Sun <murphysunx@gmail.com>
2023-03-11 22:32:19 +1100

    #feat: dealer-vehicle loads vehicles of dealer

--- web/src/app/features/vehicle/pages/dealer-vehicle/dealer-vehicle.component.ts
+++ web/src/app/features/vehicle/pages/dealer-vehicle/dealer-vehicle.component.ts
@@ -1,5 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { map, switchMap, tap } from 'rxjs';
 import { Vehicle } from '../../models/vehicle.model';
 import { VehicleService } from '../../services/vehicle.service';
 
@@ -9,12 +15,39 @@ import { VehicleService } from '../../services/vehicle.service';
   styleUrls: ['./dealer-vehicle.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DealerVehicleComponent {
+export class DealerVehicleComponent implements OnInit {
+  private init = false;
+
   bac!: string;
   vehicles?: Vehicle[];
 
   constructor(
     private route: ActivatedRoute,
+    private cd: ChangeDetectorRef,
     private vehicleService: VehicleService
   ) {}
+
+  ngOnInit(): void {
+    this.route.params
+      .pipe(
+        map((params) => params['bac']),
+        tap(() => (this.init = false)),
+        switchMap((bac) => this.vehicleService.getVehiclesByBac(bac))
+      )
+      .subscribe({
+        next: (vehicles) => {
+          this.vehicles = vehicles;
+          this.init = true;
+          this.cd.detectChanges();
+        },
+        error: (err) => {
+          this.init = true;
+          console.error(err);
+        },
+      });
+  }
+
+  get initalised(): boolean {
+    return this.init;
+  }
 }
---
+++ web/src/app/features/vehicle/pages/dealer-vehicle/dealer-vehicle.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+
+import { DealerVehicleComponent } from './dealer-vehicle.component';
+
+@NgModule({
+  imports: [],
+  exports: [],
+  declarations: [DealerVehicleComponent],
+})
+export class DealerVehicleModule {}
