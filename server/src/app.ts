432f9af5db0d1ccc9f25d402d92243d6c546046f
Murphy Sun <murphysunx@gmail.com>
2023-03-12 00:11:01 +1100

    #refactor: use http-proxy-middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,24 +1,15 @@
-import cors from "cors";
-import proxy from "express-http-proxy";
 import express from "express";
-import * as fs from "fs";
-import path from "path";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 const app = express();
 
 app.use(
-  cors({
-    credentials: true,
-    origin: "http://localhost:4200",
-  })
-);
-
-app.use(
-  "/",
-  proxy("https://bb61co4l22.execute-api.us-west-2.amazonaws.com", {
-    proxyReqPathResolver: function (req) {
-      // console.log("req path", req.path);
-      return "/development/" + req.path;
+  createProxyMiddleware({
+    target:
+      "https://bb61co4l22.execute-api.us-west-2.amazonaws.com/development",
+    changeOrigin: true,
+    onProxyRes: function (proxyRes, req, res) {
+      proxyRes.headers["access-control-allow-origin"] = "http://localhost:4200";
     },
   })
 );
