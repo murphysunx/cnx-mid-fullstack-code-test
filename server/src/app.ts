078c87f0e1cfb1c9e9466211cf405bb3c7114185
Murphy Sun <murphysunx@gmail.com>
2023-03-12 12:13:24 +1100

    #deply: to aws labmda using claudiajs

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,15 +3,29 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 
 const app = express();
 
-app.use(
+app.get(
+  "/dealers",
   createProxyMiddleware({
     target:
       "https://bb61co4l22.execute-api.us-west-2.amazonaws.com/development",
     changeOrigin: true,
     onProxyRes: function (proxyRes, req, res) {
-      proxyRes.headers["access-control-allow-origin"] = "http://localhost:4200";
+      proxyRes.headers["access-control-allow-origin"] = "*";
+    },
+  })
+);
+
+app.get(
+  "/vehicles/:bac",
+  createProxyMiddleware({
+    target:
+      "https://bb61co4l22.execute-api.us-west-2.amazonaws.com/development",
+    changeOrigin: true,
+    onProxyRes: function (proxyRes, req, res) {
+      proxyRes.headers["access-control-allow-origin"] = "*";
     },
   })
 );
 
 export default app;
+module.exports = app;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,6 @@
 import app from "./app";
 
 const port = 3000;
-
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
 });
